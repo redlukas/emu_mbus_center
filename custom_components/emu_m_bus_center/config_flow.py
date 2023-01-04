b0765f34dd19d2d8eccfc27e03f796ccd3f94532
Matthias Studer <matthiaslukas.studer@students.bfh.ch>
2023-01-04 15:17:45 +0100

    first go at config flow

---
+++ custom_components/emu_m_bus_center/config_flow.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import logging
+
+import voluptuous as vol
+from homeassistant import config_entries
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class CenterConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    _LOGGER.warning("Flow called")
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
+
+    async def async_step_user(self, user_input=None):
+        _LOGGER.warning("step called")
+        if user_input is not None:
+            _LOGGER.warning(f"user input is {user_input}")
+            return self.async_create_entry(
+                title="",
+                data={
+                    "ip": user_input.get("ip", ""),
+                    "sensors": user_input.get("sensors", ""),
+                },
+            )
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {vol.Required("ip"): str, vol.Required("sensors"): str}
+            ),
+        )
---
+++ custom_components/emu_m_bus_center/const.py
@@ -0,0 +1,3 @@
+"""Constant values for emu mbus integration."""
+
+DOMAIN = "emu_m_bus_center"
