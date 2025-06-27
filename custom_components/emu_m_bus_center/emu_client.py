81422716e4e5ff4e3a0fe70813afcf77d79fd8de
redlukas <redlukas@users.noreply.github.com>
2025-06-27 14:14:48 +0200

    make the _extract_values method static (#370)

--- custom_components/emu_m_bus_center/emu_client.py
+++ custom_components/emu_m_bus_center/emu_client.py
@@ -147,6 +147,7 @@ class EmuApiClient:
                         _LOGGER.error(
                             "Sensor %i did not supply a proper serial number", sensor_id
                         )
+            # ruff: noqa: PERF203
             except requests.exceptions.ConnectionError:
                 _LOGGER.debug("No Sensor on ID %s", sensor_id)
             except json.decoder.JSONDecodeError:
